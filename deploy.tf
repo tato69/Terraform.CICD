8fc52f88e2d15dcedb157f7af588f9786f06be37
root <root@myVM.433xgh1afajuvgs4yzzsuqz3uc.ax.internal.cloudapp.net>
2017-12-12 18:42:37 +0000

    jenkins working finally

--- deploy.tf
+++ deploy.tf
@@ -1,6 +1,15 @@
 resource "azurerm_resource_group" "test" {
-  name     = "CI-CD-pipeline-test"
+  name     = "CI-CD-pipeline-test3"
   location = "West US 2"
+}
+
+
+resource "azurerm_public_ip" "test" {
+  name                         = "CI-CD-pipeline-test-pip01"
+  location                     = "West US 2"
+  resource_group_name          = "${azurerm_resource_group.test.name}"
+  public_ip_address_allocation = "static"
+
 }
 
 resource "azurerm_virtual_network" "test" {
@@ -8,7 +17,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
   location            = "West US 2"
   resource_group_name = "${azurerm_resource_group.test.name}"
-  dns_servers         = ["10.0.2.4" , "8.8.8.8"]
 }
 
 resource "azurerm_subnet" "test" {
@@ -18,21 +26,7 @@ resource "azurerm_subnet" "test" {
   address_prefix       = "10.0.2.0/24"
 }
 
-
-
-
-
-
-
-
-resource "azurerm_public_ip" "test" {
-  name                         = "CI-CD-pipeline-test-pip01"
-  location                     = "West US 2"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  public_ip_address_allocation = "static"
-}
-
-resource "azurerm_network_interface" "test" {
+resource "azurerm_network_interface" "jenkins" {
   name                = "CI-CD-pipeline-test-nic01"
   location            = "West US 2"
   resource_group_name = "${azurerm_resource_group.test.name}"
@@ -46,11 +40,14 @@ resource "azurerm_network_interface" "test" {
   }
 }
 
+
+
+
 resource "azurerm_virtual_machine" "jenkins" {
   name                  = "CI-CD-pipeline-test-vm01"
   location              = "West US 2"
   resource_group_name   = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test.id}"]
+  network_interface_ids = ["${azurerm_network_interface.jenkins.id}"]
   vm_size               = "Standard_DS1_v2"
 
 storage_os_disk {
@@ -70,7 +67,7 @@ storage_image_reference {
 os_profile {
     computer_name  = "CI-CD-pipeline-test-vm01"
     admin_username = "ariso001a"
-	admin_password = "Password123"
+    admin_password = "Password123"
 }
 
 # Uncomment this line to delete the OS disk automatically when deleting the VM
@@ -78,94 +75,33 @@ delete_os_disk_on_termination = true
 
 os_profile_linux_config {
     disable_password_authentication = false
-}
-
-provisioner "file" {
-  source      = "./pp_agent_jenkins.bash"
-  destination = "/tmp/pp_agent_jenkins.bash"
-}
-
-provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/pp_agent_jenkins.bash",
-    "/tmp/pp_agent_jenkins.bash",
-  ]
+    ssh_keys {
+        path     = "/home/ariso001a/.ssh/authorized_keys"
+        key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI4F5FUcgWYxixSkZLmyi5KpyLWAQhy9+eCvGhUmEATWYoIyrgESKKg3pQKD/YafivPo49oA6pkqIJRo3QjyUTBYDpzOKwwkldiAvGwoZyrZKsHAfXy6iQhni6d95kErqqvL82XlYqIaZGx49adHvrZkbuG2XrlLXTOXZYb8L6PfExekyYdkxhgH51a9pnDX08cf59AlgEpSi/evrdRgSgsIm9L8I9CyaHFsOgL04aXFj2+AxTcqaKdnVt0xGfbwxwqNi8o9aANRP3+NvB8/0QCLw1uUXQrV1K0PB/qGNqYjFzgeY0Xq6fTbVFu4uQ9h5596I531IN4dY1mW6mf71p"
+    }
 }
 
 }
 
+resource "azurerm_virtual_machine_extension" "customscript" {
+name = "CustomscriptExtension"
+location = "West US 2"
+resource_group_name = "${azurerm_resource_group.test.name}"
+virtual_machine_name = "${azurerm_virtual_machine.jenkins.name}"
+publisher = "Microsoft.Azure.Extensions"
+type = "CustomScript"
+type_handler_version = "2.0"
 
-resource "azurerm_public_ip" "test2" {
-  name                         = "CI-CD-pipeline-test-pip02"
-  location                     = "West US 2"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
-  public_ip_address_allocation = "static"
-
+settings = <<SETTINGS
+{
+"fileUris": ["https://raw.githubusercontent.com/tato69/Terraform.CICD/master/pp_agent_jenkins.bash"],
+"commandToExecute": "sudo ./pp_agent_jenkins.bash"
 }
-
-resource "azurerm_network_interface" "test2" {
-  name                = "CI-CD-pipeline-test-nic02"
-  location            = "West US 2"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-
-  ip_configuration {
-    name                          = "CI-CD-pipeline-test-conf02"
-    subnet_id                     = "${azurerm_subnet.test.id}"
-    private_ip_address_allocation = "static"
-    private_ip_address = "10.0.2.5"
-    public_ip_address_id          = "${azurerm_public_ip.test2.id}"
-  }
-}
-
-resource "azurerm_virtual_machine" "apache" {
-  name                  = "CI-CD-pipeline-test-vm02"
-  location              = "West US 2"
-  resource_group_name   = "${azurerm_resource_group.test.name}"
-  network_interface_ids = ["${azurerm_network_interface.test2.id}"]
-  vm_size               = "Standard_DS1_v2"
-
-storage_os_disk {
-    name              = "CI-CD-pipeline-test-disk02"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Premium_LRS"
-}
-
-storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "16.04.0-LTS"
-    version   = "latest"
-}
-
-os_profile {
-    computer_name  = "CI-CD-pipeline-test-vm02"
-    admin_username = "ariso001a"
-	admin_password = "Password123"
-}
-
-# Uncomment this line to delete the OS disk automatically when deleting the VM
-delete_os_disk_on_termination = true
-
-os_profile_linux_config {
-    disable_password_authentication = false
-}
-
-provisioner "file" {
-  source      = "./pp_agent_apache.bash"
-  destination = "/tmp/pp_agent_apache.bash"
-}
-
-provisioner "remote-exec" {
-  inline = [
-    "chmod +x /tmp/pp_agent_apache.bash",
-    "/tmp/pp_agent_apache.bash",
-  ]
-}
-
+SETTINGS
+#closing VM
 }
 
 
-
-
-
+output "jenkins_public_ip" {
+value = "${azurerm_public_ip.test.ip_address}"
+}
