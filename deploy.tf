41e856b2b5616b7110818fa49a1b23a01ae1bdd6
Antonio Riso <ballacoipruppi@hotmail.com>
2017-12-12 22:03:15 +0100

    First full test

--- deploy.tf
+++ deploy.tf
@@ -1,42 +1,60 @@
-resource "azurerm_resource_group" "test" {
-  name     = "CI-CD-pipeline-test3"
+resource "azurerm_resource_group" "CICD-rg" {
+  name     = "CICD-rg"
   location = "West US 2"
 }
 
 
-resource "azurerm_public_ip" "test" {
-  name                         = "CI-CD-pipeline-test-pip01"
+resource "azurerm_public_ip" "jenkins" {
+  name                         = "CICD-pip-jenkins01"
   location                     = "West US 2"
-  resource_group_name          = "${azurerm_resource_group.test.name}"
+  resource_group_name          = "${azurerm_resource_group.CICD-rg.name}"
   public_ip_address_allocation = "static"
 
 }
 
-resource "azurerm_virtual_network" "test" {
-  name                = "CI-CD-pipeline-test-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = "West US 2"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+resource "azurerm_public_ip" "apache" {
+  name                         = "CICD-pip-apache01"
+  location                     = "West US 2"
+  resource_group_name          = "${azurerm_resource_group.CICD-rg.name}"
+  public_ip_address_allocation = "static"
+
 }
 
-resource "azurerm_subnet" "test" {
-  name                 = "CI-CD-pipeline-test-sub"
-  resource_group_name  = "${azurerm_resource_group.test.name}"
-  virtual_network_name = "${azurerm_virtual_network.test.name}"
+resource "azurerm_public_ip" "jdk" {
+  name                         = "CICD-pip01-jdk01"
+  location                     = "West US 2"
+  resource_group_name          = "${azurerm_resource_group.CICD-rg.name}"
+  public_ip_address_allocation = "static"
+
+}
+
+resource "azurerm_virtual_network" "CICD-net" {
+  name                = "CICD-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = "West US 2"
+  resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
+}
+
+resource "azurerm_subnet" "CICD-sub" {
+  name                 = "CICD-sub"
+  resource_group_name  = "${azurerm_resource_group.CICD-rg.name}"
+  virtual_network_name = "${azurerm_virtual_network.CICD-net.name}"
   address_prefix       = "10.0.2.0/24"
 }
 
+
+
 resource "azurerm_network_interface" "jenkins" {
-  name                = "CI-CD-pipeline-test-nic01"
+  name                = "CICD-nic-jenkins01"
   location            = "West US 2"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
 
   ip_configuration {
-    name                          = "CI-CD-pipeline-test-conf01"
-    subnet_id                     = "${azurerm_subnet.test.id}"
+    name                          = "CICD-conf-jenkins01"
+    subnet_id                     = "${azurerm_subnet.CICD-sub.id}"
     private_ip_address_allocation = "static"
     private_ip_address = "10.0.2.4"
-    public_ip_address_id          = "${azurerm_public_ip.test.id}"
+    public_ip_address_id          = "${azurerm_public_ip.jenkins.id}"
   }
 }
 
@@ -44,14 +62,14 @@ resource "azurerm_network_interface" "jenkins" {
 
 
 resource "azurerm_virtual_machine" "jenkins" {
-  name                  = "CI-CD-pipeline-test-vm01"
+  name                  = "CICD-vm-jenkins01"
   location              = "West US 2"
-  resource_group_name   = "${azurerm_resource_group.test.name}"
+  resource_group_name   = "${azurerm_resource_group.CICD-rg.name}"
   network_interface_ids = ["${azurerm_network_interface.jenkins.id}"]
   vm_size               = "Standard_DS1_v2"
 
 storage_os_disk {
-    name              = "CI-CD-pipeline-test-disk01"
+    name              = "CICD-disk-jenkins01"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Premium_LRS"
@@ -65,7 +83,7 @@ storage_image_reference {
 }
 
 os_profile {
-    computer_name  = "CI-CD-pipeline-test-vm01"
+    computer_name  = "CICD-vm-jenkins01"
     admin_username = "ariso001a"
     admin_password = "Password123"
 }
@@ -83,10 +101,160 @@ os_profile_linux_config {
 
 }
 
+
+resource "azurerm_network_interface" "apache" {
+  name                = "CICD-nic-apache01"
+  location            = "West US 2"
+  resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
+
+  ip_configuration {
+    name                          = "CICD-conf-apache01"
+    subnet_id                     = "${azurerm_subnet.CICD-sub.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address = "10.0.2.4"
+    public_ip_address_id          = "${azurerm_public_ip.apache.id}"
+  }
+}
+
+
+
+
+resource "azurerm_virtual_machine" "apache" {
+  name                  = "CICD-vm-apache01"
+  location              = "West US 2"
+  resource_group_name   = "${azurerm_resource_group.CICD-rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.apache.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+storage_os_disk {
+    name              = "CICD-disk-apache01"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+}
+
+storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+}
+
+os_profile {
+    computer_name  = "CICD-vm-apache01"
+    admin_username = "ariso001a"
+    admin_password = "Password123"
+}
+
+# Uncomment this line to delete the OS disk automatically when deleting the VM
+delete_os_disk_on_termination = true
+
+os_profile_linux_config {
+    disable_password_authentication = false
+    ssh_keys {
+        path     = "/home/ariso001a/.ssh/authorized_keys"
+        key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI4F5FUcgWYxixSkZLmyi5KpyLWAQhy9+eCvGhUmEATWYoIyrgESKKg3pQKD/YafivPo49oA6pkqIJRo3QjyUTBYDpzOKwwkldiAvGwoZyrZKsHAfXy6iQhni6d95kErqqvL82XlYqIaZGx49adHvrZkbuG2XrlLXTOXZYb8L6PfExekyYdkxhgH51a9pnDX08cf59AlgEpSi/evrdRgSgsIm9L8I9CyaHFsOgL04aXFj2+AxTcqaKdnVt0xGfbwxwqNi8o9aANRP3+NvB8/0QCLw1uUXQrV1K0PB/qGNqYjFzgeY0Xq6fTbVFu4uQ9h5596I531IN4dY1mW6mf71p"
+    }
+}
+
+}
+
+resource "azurerm_network_interface" "jdk" {
+  name                = "CICD-nic-jdk01"
+  location            = "West US 2"
+  resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
+
+  ip_configuration {
+    name                          = "CICD-conf-jdk01"
+    subnet_id                     = "${azurerm_subnet.CICD-sub.id}"
+    private_ip_address_allocation = "static"
+    private_ip_address = "10.0.2.4"
+    public_ip_address_id          = "${azurerm_public_ip.jdk.id}"
+  }
+}
+
+
+
+
+resource "azurerm_virtual_machine" "jdk" {
+  name                  = "CICD-vm-jdk01"
+  location              = "West US 2"
+  resource_group_name   = "${azurerm_resource_group.CICD-rg.name}"
+  network_interface_ids = ["${azurerm_network_interface.jdk.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+storage_os_disk {
+    name              = "CICD-disk-jdk01"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
+}
+
+storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04.0-LTS"
+    version   = "latest"
+}
+
+os_profile {
+    computer_name  = "CICD-vm-jdk01"
+    admin_username = "ariso001a"
+    admin_password = "Password123"
+}
+
+# Uncomment this line to delete the OS disk automatically when deleting the VM
+delete_os_disk_on_termination = true
+
+os_profile_linux_config {
+    disable_password_authentication = false
+    ssh_keys {
+        path     = "/home/ariso001a/.ssh/authorized_keys"
+        key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDI4F5FUcgWYxixSkZLmyi5KpyLWAQhy9+eCvGhUmEATWYoIyrgESKKg3pQKD/YafivPo49oA6pkqIJRo3QjyUTBYDpzOKwwkldiAvGwoZyrZKsHAfXy6iQhni6d95kErqqvL82XlYqIaZGx49adHvrZkbuG2XrlLXTOXZYb8L6PfExekyYdkxhgH51a9pnDX08cf59AlgEpSi/evrdRgSgsIm9L8I9CyaHFsOgL04aXFj2+AxTcqaKdnVt0xGfbwxwqNi8o9aANRP3+NvB8/0QCLw1uUXQrV1K0PB/qGNqYjFzgeY0Xq6fTbVFu4uQ9h5596I531IN4dY1mW6mf71p"
+    }
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 resource "azurerm_virtual_machine_extension" "customscript" {
 name = "CustomscriptExtension"
 location = "West US 2"
-resource_group_name = "${azurerm_resource_group.test.name}"
+resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
 virtual_machine_name = "${azurerm_virtual_machine.jenkins.name}"
 publisher = "Microsoft.Azure.Extensions"
 type = "CustomScript"
@@ -102,6 +270,90 @@ SETTINGS
 }
 
 
-output "jenkins_public_ip" {
-value = "${azurerm_public_ip.test.ip_address}"
+
+resource "azurerm_virtual_machine_extension" "apache" {
+name = "CustomscriptExtension"
+location = "West US 2"
+resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
+virtual_machine_name = "${azurerm_virtual_machine.apache.name}"
+publisher = "Microsoft.Azure.Extensions"
+type = "CustomScript"
+type_handler_version = "2.0"
+
+settings = <<SETTINGS
+{
+"fileUris": ["https://raw.githubusercontent.com/tato69/Terraform.CICD/master/pp_agent_apache.bash"],
+"commandToExecute": "sudo ./pp_agent_apache.bash"
 }
+SETTINGS
+#closing VM
+}
+
+
+output "jdk_public_ip" {
+value = "${azurerm_public_ip.jdk.ip_address}"
+}
+
+
+resource "azurerm_virtual_machine_extension" "jdk" {
+name = "CustomscriptExtension"
+location = "West US 2"
+resource_group_name = "${azurerm_resource_group.CICD-rg.name}"
+virtual_machine_name = "${azurerm_virtual_machine.jdk.name}"
+publisher = "Microsoft.Azure.Extensions"
+type = "CustomScript"
+type_handler_version = "2.0"
+
+settings = <<SETTINGS
+{
+"fileUris": ["https://raw.githubusercontent.com/tato69/Terraform.CICD/master/pp_agent_jdk.bash"],
+"commandToExecute": "sudo ./pp_agent_jdk.bash"
+}
+SETTINGS
+#closing VM
+}
+
+
+
+
+
+
+
+
+
+
+output "jenkins_public_ip" {
+value = "${azurerm_public_ip.jenkins.ip_address}"
+}
+
+output "apache_public_ip" {
+value = "${azurerm_public_ip.apache.ip_address}"
+}
+
+output "jdk_public_ip" {
+value = "${azurerm_public_ip.jdk.ip_address}"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pp_agent_jdk.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+# 
+# This Script delivers the following configuration:
+#    Sets Puppet facts to dev_dev environment (Dev Stamp Dev branch)
+#    DNS resolves to devcloudnp.ad.pwcinternal.com
+#    Yum dev repos used
+#    Connects to Dev Puppet Master
+#
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/opt/puppetlabs/bin:/root/bin
+
+factfile='/etc/puppetlabs/facter/facts.d/site-facts.yaml'
+factdir='/etc/puppetlabs/facter/facts.d/'
+
+echo "Setting ${factfile}"
+
+mkdir -p ${factdir}
+echo "pwcenv: dev_explore" > ${factfile}
+#echo "pwcdatacenter: azure" >> ${factfile}
+#echo "pwcdomain: devcloudnp.ad.pwcinternal.com" >> ${factfile}
+
+if [ -d /var/www/html/RH ];then
+
+  echo "pwcrepo: redhat7repo" >> ${factfile}
+
+elif [ -d /var/www/html/CentOS ]; then
+  
+  echo "pwcrepo: centos7repo" >> ${factfile}
+
+fi
+
+yum clean all
+
+#  Puppet Agent Install
+echo "Checking for Puppet Agent"
+hasPuppHost=$(egrep -q '^104.196.135.96 96.135.196.104.bc.googleusercontent.com bob-pupp-explore-master.c.pg-us-e-rob-01.internal bob-pupp-explore-master' /etc/hosts)${?}
+
+if [ ${hasPuppHost} -ne 0 ] ; then
+  echo "Installing Puppet Agent"
+  echo '104.196.135.96 96.135.196.104.bc.googleusercontent.com bob-pupp-explore-master.c.pg-us-e-rob-01.internal bob-pupp-explore-master # Puppet Master' >> /etc/hosts
+fi
+
+if [ ! -h /usr/local/bin/puppet ] ; then 
+  curl -k https://bob-pupp-explore-master.c.pg-us-e-rob-01.internal:8140/packages/current/install.bash | bash
+fi
+
+
+apt-get install -y git
+mkdir -p /tmp/puppet/
+git clone https://github.com/tato69/jdkariso001a /tmp/puppet/jdkariso001a
+puppet module install puppetlabs-vcsrepo --modulepath=/tmp/puppet/
+puppet apply --modulepath=/tmp/puppet/ -e 'include jdkariso001a'
